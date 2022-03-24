3838231a6c1238191d47b4ec8c1d1627b2ba9cb1
Lacramioara <lacramioara.oloeriu@gmail.com>
2022-03-24 20:24:40 +0100

    fix VStack

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import {
   theme,
   Container,
   Flex,
+  Heading,
 } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import { Logo } from "./Logo";
@@ -18,13 +19,13 @@ export const App = () => (
   <ChakraProvider theme={theme}>
     <Container maxW="container.xl" p={0}>
       <Flex h="100vh" py={20}>
-        <VStack
-          w="full"
-          h="full"
-          p={20}
-          spacing={10}
-          alignItems="flex-start"
-        ></VStack>
+        <VStack w="full" h="full" p={20} spacing={10} alignItems="flex-start">
+          <VStack spacing={3} alignItems="flex-start">
+            <Heading size="2x1"> Your details</Heading>
+            <Text> If you already have an account , click here </Text>
+          </VStack>
+        </VStack>
+
         <VStack
           w="full"
           h="full"
