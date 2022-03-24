720d238737a53eb4d93a07bfe67db8b4473bd057
Lacramioara <lacramioara.oloeriu@gmail.com>
2022-03-24 19:39:58 +0100

    built de Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import * as React from "react";
 import {
   ChakraProvider,
   Box,
@@ -8,31 +8,32 @@ import {
   Code,
   Grid,
   theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+  Container,
+  Flex,
+} from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { Logo } from "./Logo";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
+    <Container maxW="container.xl" p={0}>
+      <Flex h="100vh" py={20}>
+        <VStack
+          w="full"
+          h="full"
+          p={20}
+          spacing={10}
+          alignItems="flex-start"
+        ></VStack>
+        <VStack
+          w="full"
+          h="full"
+          p={20}
+          spacing={10}
+          alignItems="flex-start"
+          bg="gray.50"
+        ></VStack>
+      </Flex>
+    </Container>
   </ChakraProvider>
-)
+);
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import { App } from "./App"
 import reportWebVitals from "./reportWebVitals"
 import * as serviceWorker from "./serviceWorker"
 
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <ColorModeScript />
@@ -22,3 +25,4 @@ serviceWorker.unregister()
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
+ 
