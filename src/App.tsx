955504e324e60afabcf0f39010f85b1dcf1349ab
Lacramioara <lacramioara.oloeriu@gmail.com>
2022-03-25 09:38:26 +0100

    build details form

--- src/App.tsx
+++ src/App.tsx
@@ -14,27 +14,16 @@ import {
 } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import { Logo } from "./Logo";
+import Cart from '../src/cart'
+import Details from '../src/details'
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Container maxW="container.xl" p={0}>
       <Flex h="100vh" py={20}>
-        <VStack w="full" h="full" p={20} spacing={10} alignItems="flex-start">
-          <VStack spacing={3} alignItems="flex-start">
-            <Heading size="2x1"> Your details</Heading>
-            <Text> If you already have an account , click here </Text>
-          </VStack>
-        </VStack>
-
-        <VStack
-          w="full"
-          h="full"
-          p={20}
-          spacing={10}
-          alignItems="flex-start"
-          bg="gray.50"
-        ></VStack>
+        <Details/>
+        <Cart />
       </Flex>
     </Container>
   </ChakraProvider>
-);
+)
---
+++ src/details.tsx
@@ -0,0 +1,73 @@
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  VStack,
+  Heading,
+  Text,
+  SimpleGrid,
+  Grid,
+  GridItem,
+  Select,
+  Checkbox,
+  Button,
+} from "@chakra-ui/react";
+
+const Details = () => {
+  return (
+    <VStack w="full" h="full" p={20} spacing={10} alignItems="flex-start">
+      <VStack spacing={3} alignItems="flex-start">
+        <Heading size="2x1"> Your details</Heading>
+        <Text> If you already have an account , click here </Text>
+      </VStack>
+      <SimpleGrid columns={2} columnGap={3} rowGap={6} w="full">
+        <GridItem colSpan={1}>
+          <FormControl>
+            <FormLabel>First Name</FormLabel>
+            <Input placeholder="Lacra" />
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <FormControl>
+            <FormLabel>Last Name</FormLabel>
+            <Input placeholder="Oloeriu" />
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <FormControl>
+            <FormLabel>Adress</FormLabel>
+            <Input placeholder="Blaisantvest 9" />
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <FormControl>
+            <FormLabel>City</FormLabel>
+            <Input placeholder="Ghent" />
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={1}>
+          <FormControl>
+            <FormLabel>Country</FormLabel>
+            <Select>
+              <option value="usa">United State of America</option>
+              <option value="be">Belgium</option>
+              <option value="ro">Roumania</option>
+              <option value="de">Germany</option>
+            </Select>
+          </FormControl>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <Checkbox defaultChecked>Ship to billing address.</Checkbox>
+        </GridItem>
+
+        <GridItem colSpan={2}>
+          <Button size="lg" w="full">
+            Place Order
+          </Button>
+        </GridItem>
+      </SimpleGrid>
+    </VStack>
+  );
+};
+
+export default Details;
